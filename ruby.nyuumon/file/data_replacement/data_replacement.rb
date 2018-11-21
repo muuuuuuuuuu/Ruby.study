7dba27e8ea2f2a5c556e0b23177c6a9aa0b011e0
katuya nakamura <kakatutuyaya.0106@icloud.com>
2018-11-21 20:58:18 +0900

    data_replacement.rb

---
+++ ruby.nyuumon/file/data_replacement/data_replacement.rb
@@ -0,0 +1,19 @@
+#データを書き換えるクラス
+
+class DataReplacement
+  attr_writer :inputfile, :outputfile
+  #データを読み込むための処理
+  def replace
+    File.read(@inputfile).split("\n").each do |content|
+      p content
+    end
+  end
+end
+
+replacement = DataReplacement.new
+#入力ファイルの指定
+replacement.inputfile = ARGV[0]
+#出力ファイルの指定
+replacement.outputfile = ARGV[1]
+#データ書き換え
+replacement.replace
