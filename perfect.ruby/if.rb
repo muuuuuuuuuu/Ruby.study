63944ad718fda4cb757780cc3012b25690499ead
katuya nakamura <kakatutuyaya.0106@icloud.com>
2018-11-24 23:12:52 +0900

    こっちも?

--- perfect.ruby/if.rb
+++ perfect.ruby/if.rb
@@ -1,27 +1,40 @@
-# puts "今は何月ですか？"
-# month = gets.to_i
-# if month == 11
-#   puts "正解です"
-# else
-#   puts "違うわぼけ！！"
-# end
+puts "今は何月ですか？"
+month = gets.to_i
+if month == 11
+  puts "正解です"
+else
+  puts "違うわぼけ！！"
+end
 
-# --------------------------------
+--------------------------------
 
-# n = gets.to_i
-#
-# if n == 1
-#   puts "6"
-# elsif n == 6
-#   puts "1"
-# elsif n == 2
-#   puts "5"
-# elsif n == 5
-#   puts "2"
-# elsif n == 3
-#   puts "4"
-# else
-#   puts "3"
-# end
+n = gets.to_i
 
-# --------------------------------------
+if n == 1
+  puts "6"
+elsif n == 6
+  puts "1"
+elsif n == 2
+  puts "5"
+elsif n == 5
+  puts "2"
+elsif n == 3
+  puts "4"
+else
+  puts "3"
+end
+
+--------------------------------------
+差額を求める
+
+a = "1000000007"
+c = a.length
+#cは数値
+b = gets.chomp
+d = b.length
+#dも数値
+if c < d
+  p d - c
+else
+  p c - d
+end
--- perfect.ruby/insert.rb
+++ perfect.ruby/insert.rb
@@ -12,3 +12,11 @@ print y[4]
 print y[5]
 1234/5/6
 -----------------------------------------
+顔文字を作る
+
+a = gets.chomp
+b = gets.chomp
+c = ["(", ")", "/"]
+c.insert(1,a,b,a)
+
+puts c.join
