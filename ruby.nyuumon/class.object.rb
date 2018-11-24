a300c575c035fa2c162a75b85896b0d6e29a12fe
katuya nakamura <kakatutuyaya.0106@icloud.com>
2018-11-24 21:56:13 +0900

    出来なかった

--- ruby.nyuumon/class.object.rb
+++ ruby.nyuumon/class.object.rb
@@ -1,11 +1,11 @@
 #スペースを空けて入力する
-a = gets.chomp.split(" ")
-b = gets.chomp.split(" ")
-c = gets.chomp.split(" ")
+# a = gets.chomp.split(" ")
+# b = gets.chomp.split(" ")
+# c = gets.chomp.split(" ")
 #配列の文字列を数値にする
-aa = a.map(&:to_i)
-bb = b.map(&:to_i)
-cc = c.map(&:to_i)
+# aa = a.map(&:to_i)
+# bb = b.map(&:to_i)
+# cc = c.map(&:to_i)
 
 # if aa[0] > bb[0] > cc[0] && cc[1] > bb[1] > aa[1]
 # puts "ABC"
@@ -32,39 +32,89 @@ cc = c.map(&:to_i)
 #   puts "ダメ"
 # end
 
-if (aa[0] > bb[0] && bb[0] > cc[0])
-  if (aa[0] == bb[0] || bb[0] == cc[0] || aa[0] == cc[0])
-  else(cc[1] >= bb[1] && bb[1] >= aa[1]) || (bb[1] >= cc[1] && cc[1] >= aa[1])
-      puts "ABC"
+# if (aa[0] > bb[0] && bb[0] > cc[0])
+#   if (aa[0] == bb[0] || bb[0] == cc[0] || aa[0] == cc[0])
+#   else(cc[1] >= bb[1] && bb[1] >= aa[1]) || (bb[1] >= cc[1] && cc[1] >= aa[1])
+#       puts "ABC"
+#   end
+# end
+# if (aa[0] > cc[0] || cc[0] > bb[0])
+#   if (aa[0] == cc[0] || cc[0] == bb[0] || aa[0] == bb[0])
+#   else(bb[1] >= cc[1] && cc[1] >= aa[1]) || (cc[1] >= bb[1] && bb[1] >= aa[1])
+#       puts "ACB"
+#   end
+# end
+# if (bb[0] > aa[0] && aa[0] > cc[0])
+#   if (bb[0] == aa[0] || aa[0] == cc[0] || bb[0] || cc[0])
+#   else(cc[1] >= aa[1] && aa[1] >= bb[1]) || (aa[1] >= cc[1] && cc[1] >= bb[1])
+#       puts "BAC"
+#   end
+# end
+# if (bb[0] > cc[0] && cc[0] > aa[0])
+#   if (bb[0] == cc[0] || cc[0] == aa[0] || bb[0] == aa[0])
+#   else(aa[1] >= cc[1] && cc[1] >= bb[1]) || (cc[1] >= aa[1] && aa[1] >= bb[1])
+#       puts "BCA"
+#   end
+# end
+# if (cc[0] > aa[0] && aa[0] > bb[0])
+#   if (cc[0] == aa[0] || aa[0] == bb[0] || cc[0] == bb[0])
+#   else(bb[1] >= aa[1] && aa[1] >= cc[1]) || (aa[1] >= bb[1] && bb[1] >= cc[1])
+#       puts "CAB"
+#   end
+# end
+# if (cc[0] > bb[0] && bb[0] > aa[0])
+#   if (cc[0] == bb[0] && bb[0] == aa[0])
+#   else(aa[1] >= bb[1] && bb[1] >= cc[1]) || (bb[1] >= aa[1] && aa[1] >= cc[1])
+#       puts "CBA"
+#   end
+# end
+
+
+
+a = gets.chomp.split(" ")
+b = gets.chomp.split(" ")
+c = gets.chomp.split(" ")
+# 配列の文字列を数値にする
+aa = a.map(&:to_i)
+bb = b.map(&:to_i)
+cc = c.map(&:to_i)
+
+d = aa + bb + cc
+e = d.sort
+p e
+  class Keisan
+    def tai(e)
+      if (e[0] == e[1] && e[0] == e[2] && e[1] == e[2])
+        p e[0..2] = 3
+      elsif e[0] == e[1]
+        p e[0..1] = 3
+        p e[2] = 2
+      elsif e[1] == e[2]
+        p e[0] = 3
+        p e[1..2] = 2
+      else
+        p e[0] = 3
+        p e[1] = 2
+        p e[2] = 1
+      end
+    end
+
+    def height(e)
+      if (e[3] == e[4] && e[3] == e[5] && e[4] == e[5])
+        p e[3..5] = 3 * 10
+      elsif e[3] == e[4]
+        p e[3..4] = 2 * 10
+        p e[5] = 3 * 10
+      elsif e[4] == e[5]
+        p e[3] = 2 * 10
+        p e[4..5] = 3 * 10
+      else
+        p e[3] = 1 * 10
+        p e[4] = 2 * 10
+        p e[5] = 3 * 10
+      end
+    end
   end
-end
-if (aa[0] > cc[0] || cc[0] > bb[0])
-  if (aa[0] == cc[0] || cc[0] == bb[0] || aa[0] == bb[0])
-  else(bb[1] >= cc[1] && cc[1] >= aa[1]) || (cc[1] >= bb[1] && bb[1] >= aa[1])
-      puts "ACB"
-  end
-end
-if (bb[0] > aa[0] && aa[0] > cc[0])
-  if (bb[0] == aa[0] || aa[0] == cc[0] || bb[0] || cc[0])
-  else(cc[1] >= aa[1] && aa[1] >= bb[1]) || (aa[1] >= cc[1] && cc[1] >= bb[1])
-      puts "BAC"
-  end
-end
-if (bb[0] > cc[0] && cc[0] > aa[0])
-  if (bb[0] == cc[0] || cc[0] == aa[0] || bb[0] == aa[0])
-  else(aa[1] >= cc[1] && cc[1] >= bb[1]) || (cc[1] >= aa[1] && aa[1] >= bb[1])
-      puts "BCA"
-  end
-end
-if (cc[0] > aa[0] && aa[0] > bb[0])
-  if (cc[0] == aa[0] || aa[0] == bb[0] || cc[0] == bb[0])
-  else(bb[1] >= aa[1] && aa[1] >= cc[1]) || (aa[1] >= bb[1] && bb[1] >= cc[1])
-      puts "CAB"
-  end
-end
-if (cc[0] > bb[0] && bb[0] > aa[0])
-  if (cc[0] == bb[0] && bb[0] == aa[0])
-  else(aa[1] >= bb[1] && bb[1] >= cc[1]) || (bb[1] >= aa[1] && aa[1] >= cc[1])
-      puts "CBA"
-  end
-end
+f = Keisan.new
+p f.tai(e)
+p f.height(e)
